6241415c338c5354adf15add5317fc26db96d3c4
shuangya <896640547@qq.com>
2014-08-18 15:20:48 +0800

    同步分支Server和Master

--- www/data/db.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-// define('ENKEY','Wfmj9wYxgc');
-define('SQLHOST','localhost:3306');
-define('SQLUSER','root');
-define('SQLPWD','123456');
-define('SQLNAME','mynode');
-define('DBPREFIX','');
-?>
